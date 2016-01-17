ae57f11a8aba227ac1d19eabee3a01b4188946a6
coldclimate <oliver@coldclimate.co.uk>
2016-01-17 19:14:17 +0000

    ant-or-dec

---
+++ ant-or-dec/js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+	score = 0;
+	inital_image = Math.floor(Math.random() * 6) + 1;
+	$("#the_image").attr("src", "/img/"+inital_image + ".jpg");
+
+	reload = function(result, current, score) {
+		count = Math.floor(Math.random() * 6) + 1;
+		console.log(count,current)
+
+		$("#the_image").attr("src", "/img/"+count + ".jpg");
+		console.log(count + ".jpg");
+
+		$("#latest_result").text(result)
+		$("#latest_score").text(score)
+	}
+
+
+	var check_ant = function() {
+		check("ant");
+	}
+
+	var check_dec = function() {
+		check("dec");
+	}
+
+	var check = function(who) {
+		var src = $('#the_image').attr('src');
+		guess = parseInt(src.replace("/img/","").split(".")[0]);
+
+		dec = guess % 2 == 0;
+
+		if(dec){
+			if(who==="ant"){
+				score += 1;
+				sucess = true;
+			}else{
+				// failure
+				sucess = false;
+			}
+			// if the player picked ant then add it sucess
+		}else{
+			if(who==="dec"){
+				score += 1;
+				sucess = true;
+			}else{
+				sucess = false;
+			}
+		}
+
+		console.log("who:" + who);
+		console.log("guess:" + guess);
+		reload(sucess, guess, score);
+	}
+
+	$("#ant").click(check_ant);
+	$("#dec").click(check_dec);
+
+});
