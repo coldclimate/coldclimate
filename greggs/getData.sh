09e26dc9098e51d1a4774f7c7fe6d755e62d274c
coldclimate <>
2022-11-14 17:52:53 +0000

    clean up after yourself

--- greggs/getData.sh
+++ greggs/getData.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 curl  https://production-digital.greggs.co.uk/api/v1.0/shops -o greggs.json
 jq  '[.[] | {name: .shopName, lat: .address.latitude, long: .address.longitude}]' greggs.json > name-location.json
+rm  greggs.json
